2256019f6019f995d5cd76e72ba77caf409a65d8
Ndeye Fatou Diop <ndeyefatou.diop@monboreal.ca>
2024-10-18 00:31:44 +0200

    Classe livre

---
+++ ConsoleLivre/Livre.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivre
+{
+    internal class Livre
+    {
+        public string Auteur { get; set; }
+        public string Titre { get; set; }
+        public int NombrePage { get; set; }
+
+        public Livre (string auteur, string titre, int nbrpage)
+        {
+            Auteur = auteur;
+            Titre = titre;
+            NombrePage = nbrpage;
+        }
+
+        public void affichelesdetailsduLivre()
+        {
+            Console.WriteLine($"le nom de l'auteur est {Auteur}, le titre est {Titre} et a pour nombre de page {NombrePage}")
+        }
+}
+            
+    }
+
